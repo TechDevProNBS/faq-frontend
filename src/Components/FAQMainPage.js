6510a7d4a5bf57983ce41607c54d0ee471411193
Max <Kieran has a weird sense of humour>
2019-12-04 09:34:12 +0000

    Get requests for mainpage

--- src/Components/FAQMainPage.js
+++ src/Components/FAQMainPage.js
@@ -1,94 +1,147 @@
-import React from 'react';
-import { Form, FormControl} from 'react-bootstrap'
-import './css/FAQ.css'
-import { TextArea } from 'semantic-ui-react'
+    import React from 'react';
+    import { Form, FormControl } from 'react-bootstrap'
+    import './css/FAQ.css'
+    import { TextArea } from 'semantic-ui-react'
 
-const TextAreaExampleTextArea = () => (
-    <Form>
-        <TextArea placeholder='Question...' />
-    </Form>
-)
+    const TextAreaExampleTextArea = () => (
+        <Form>
+            <TextArea placeholder='Question...' />
+        </Form>
+    )
 
-export default class Home extends React.Component {
+    export default class Home extends React.Component {
+        constructor() {
+            super();
 
-    OpenQuestion = () => {
-        var form = document.getElementById('Question')
-        form.style.visibility = 'visible'
-    }
-    CloseQuestion = () => {
-        var form = document.getElementById('Question')
-        form.style.visibility = 'hidden'
-    }
+            this.state = {                                                        //this.state represent the rendered values, i.e. what’s currently on the screen
+                RecentQ: [],
+                UnansweredQ: [],
+                TopRatedQ: []
+                //Name of what you want 
+            }
 
-    render() {
+        }
+        componentDidMount = () => {
+            let dataUnanswered = ""
+            let dataRecent = ""
 
-        return (
-            <div>
+            fetch("http://localhost:4001/Questions/RecentQ")                         //Url from backend
+                .then(response => response.json())
+                .then(dataRe => {
+                    dataRecent = dataRe;
+                })
+                fetch("http://localhost:4001/Questions/UnansweredQ")                                  //Url from backend
+                .then(response => response.json())
+                .then(dataUn => {
+                    dataUnanswered = dataUn
+                })
+                fetch("http://localhost:4001/Questions/TopRatedQ")                                  //Url from backend
+                .then(response => response.json())
+                .then(dataTop => {
 
-                <body id="page-top">
+                    this.setState({
+                        RecentQ: dataRecent,
+                        UnansweredQ: dataUnanswered,
+                            TopRatedQ: dataTop
+                    })
 
-                    <nav class="navbar navbar-expand-lg fixed-top" id="mainNav" style={{ backgroundColor: '#DFDFDF', borderTop: '2px solid', borderBottom: '2px solid', color: 'black', paddingTop:'0px', paddingBottom:'0px'}}>
-                        <div class="container">
-                            <a class="navbar-brand js-scroll-trigger" href="/"><img
-                                src="Nationwide.png"
-                                width="50"
-                                height="50"
-                                alt="Nationwide Logo"
-                                style={{ borderRadius: '25px' }} /></a>
+                })
 
-                            <Form inline>
-                                <FormControl type="text" spellcheck='true' size="sm" placeholder="Search..." className="mr-sm-1" style={{ borderRadius: '25px', width:'250px' }} />
-                            </Form>
-                            <div class="collapse navbar-collapse" id="navbarResponsive">
+                
 
+        }
+        
+        
+
+
+        OpenQuestion = () => {
+            var form = document.getElementById('Question')
+            form.style.visibility = 'visible'
+        }
+        CloseQuestion = () => {
+            var form = document.getElementById('Question')
+            form.style.visibility = 'hidden'
+        }
+
+
+        render() {
+
+            return (
+                <div>
+
+                    <body id="page-top">
+
+                        <nav class="navbar navbar-expand-lg fixed-top" id="mainNav" style={{ backgroundColor: '#DFDFDF', borderTop: '2px solid', borderBottom: '2px solid', color: 'black', paddingTop: '0px', paddingBottom: '0px' }}>
+                            <div class="container">
+                                <a class="navbar-brand js-scroll-trigger" href="/"><img
+                                    src="Nationwide.png"
+                                    width="50"
+                                    height="50"
+                                    alt="Nationwide Logo"
+                                    style={{ borderRadius: '25px' }} /></a>
+
+                                <Form inline>
+                                    <FormControl type="text" spellcheck='true' size="sm" placeholder="Search..." className="mr-sm-1" style={{ borderRadius: '25px', width: '250px' }} />
+                                </Form>
+                                <div class="collapse navbar-collapse" id="navbarResponsive">
+
+                                </div>
+                            </div>
+                        </nav>
+                    </body>
+                    <br />
+
+                    <div class="container" style={{ marginTop: '60px', marginBottom: '30px' }}>
+                        <div class="row">
+                            <div class="col-lg-8  ">
+                                <h3 style={{ color: '#3252A4' }}>Trending Questions</h3>
+                                <p class="lead"></p>
+                                {
+
+                                    this.state.TopRatedQ.map((data, index) =>
+
+                                        <div>
+                                            <a href="/answer">{index + 1}) {data.question}</a><br />
+                                            <br />
+                                        </div>
+                                    )}
                             </div>
                         </div>
-                    </nav>
-                </body>
-                <br />
+                        <div class="row">
+                            <div class="col-lg-8 ">
+                                <h3 style={{ color: '#3252A4' }}>Questions Which Need Answers</h3>
+                                <p class="lead"></p>
+                                {
+                                    this.state.UnansweredQ.map((data, index) =>
+                                        <div>
+                                            <a href="/answer">{index + 1}) {data.question}</a><br />
+                                            <br />
+                                        </div>
 
-                <div class="container" style={{marginTop:'60px', marginBottom:'30px'}}>
-                    <div class="row">
-                        <div class="col-lg-8  ">
-                            <h3 style={{ color: '#3252A4' }}>Trending Questions</h3>
-                            <p class="lead"></p>
-                            <a href="/answer">1) This is Question Number 1?</a><br />
-                            <a href="/answer">2)</a><br />
-                            <a href="/answer">3)</a><br />
-                            <a href="/answer">4)</a><br />
-                            <a href="/answer">5)</a><br /><br />
+                                    )}
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-lg-8">
+                                <h3 style={{ color: '#3252A4' }}>Recently Posted Questions</h3>
+                                <p class="lead"></p>
+                                {
+                                    this.state.RecentQ.map((data, index) =>
+                                        <div>
+                                            <a href="/answer">{index+1}) {data.question}</a><br />
+                                            <br />
+                                        </div>
+                                    )}
+                            </div>
                         </div>
                     </div>
-                    <div class="row">
-                        <div class="col-lg-8 ">
-                            <h3 style={{ color: '#3252A4' }}>Questions Which Need Answers</h3>
-                            <p class="lead"></p>
-                            <a href="/answer">1)</a><br />
-                            <a href="/answer">2)</a><br />
-                            <a href="/answer">3)</a><br />
-                            <a href="/answer">4)</a><br />
-                            <a href="/answer">5)</a><br /><br />
+                    <footer class="py-1 sticky-bottom" style={{ backgroundColor: '#DFDFDF', borderTop: '2px solid', borderBottom: '2px solid', color: 'black' }}>
+                        <div class="container">
+                            <p class="m-0 text-center text-black">Copyright &copy; APT 2019</p>
                         </div>
-                    </div>
-                    <div class="row">
-                        <div class="col-lg-8">
-                            <h3 style={{ color: '#3252A4' }}>Recently Posted Questions</h3>
-                            <p class="lead"></p>
-                            <a href="/answer">1)</a><br />
-                            <a href="/answer">2)</a><br />
-                            <a href="/answer">3)</a><br />
-                            <a href="/answer">4)</a><br />
-                            <a href="/answer">5)</a><br /><br />
-                        </div>
-                    </div>
+                    </footer>
                 </div>
-                <footer class="py-1 sticky-bottom" style={{ backgroundColor: '#DFDFDF',  borderTop: '2px solid', borderBottom: '2px solid', color: 'black'}}>
-    <div class="container">
-      <p class="m-0 text-center text-black">Copyright &copy; APT 2019</p>
-    </div>
-  </footer>
-            </div>
 
-        )
+            )
+        }
     }
-}
