6c510c009d9eeed52733752338beef4f66f27661
Ryan <ryansharpe@gmail.com>
2019-12-05 11:19:46 +0000

    up and downvote buttons added

--- src/Components/AnswerPage.component.js
+++ src/Components/AnswerPage.component.js
@@ -79,6 +79,9 @@ export default class Answer extends React.Component {
                                 this.state.RecentA.map((data) =>
                                 <div>
                                       <text style={{marginLeft:'80px', backgroundColor: '#EEEEEE'}}>{data.answer}</text>
+                                      <Button variant='primary' style={{marginLeft:'20px', height:'25px', width:'20px', paddingRight:'0', paddingLeft:'0', paddingTop:'0'}}><i class="arrow up"></i></Button>
+                                      <Button variant='danger' style={{marginLeft:'5px', marginRight:'5px', height:'25px', width:'20px', paddingRight:'0', paddingLeft:'0',paddingTop:'0'}}><i class="arrow down"></i></Button>
+                                      (rating)
                                       <Button variant='danger' size='sm' style={{height: '25px', paddingTop: '0', position:'absolute', right:'40px'}}>Delete</Button><Button variant='secondary' size='sm' style={{height: '25px', paddingTop: '0', position:'absolute', right:'110px'}}>Edit</Button><br />
                             <Button variant='primary' size='sm' onClick={() => this.handleButtonToggleCommentModal(true)} style={{ height: '25px', paddingTop: '0', marginLeft: '100px' }}>Add Comment</Button>
                             <CommentModal content={this.textComment()} title={"Add A Comment"} showModal={this.state.showModal} close={() => this.handleButtonToggleCommentModal(false)} /><br />
