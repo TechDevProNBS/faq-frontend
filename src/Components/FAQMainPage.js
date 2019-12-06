ee81ae9966b93afc305dd068ffb36d6d338e1871
Oli <Olirecord@hotmail.co.uk>
2019-12-06 13:04:15 +0000

    will render home page differently if searching for questions

--- src/Components/FAQMainPage.js
+++ src/Components/FAQMainPage.js
@@ -7,6 +7,7 @@ import { TextArea } from 'semantic-ui-react'
 export default class Home extends React.Component {
     constructor() {
         super();
+        sessionStorage.setItem('search',2)
 
         this.state = {                                                        //this.state represent the rendered values, i.e. what’s currently on the screen
             RecentQ: [],
@@ -17,12 +18,32 @@ export default class Home extends React.Component {
         }
     }
 
+    search = (e) => {
+        e.preventDefault()
+        var initialString = document.getElementById("searchText").value
+        var queryString = initialString.replace(/[^a-zA-Z ]/g, "")
+        fetch(`http://localhost:4001/Questions/SearchQ/${queryString}`)
+        .then(response => response.json())
+        .then(data => {
+            this.setState({
+                filteredResults:data
+            })
+            sessionStorage.setItem('search',1)
+        })
+    }
+
+    renderHomepage(){
+        sessionStorage.setItem('search',2)
+    }
+
     Qstorage(question, q_id) {
         sessionStorage.setItem('questions', question)
         sessionStorage.setItem('q_id', q_id)
     }
 
     componentDidMount = async () => {
+      
+        
         let dataUnanswered = ""
         let dataRecent = ""
 
@@ -65,84 +86,137 @@ export default class Home extends React.Component {
 
 
     render() {
-
-        return (
-            <div>
-                <body id="page-top">
-
-                    <nav class="navbar navbar-expand-lg fixed-top" id="mainNav" style={{ backgroundColor: '#DFDFDF', borderTop: '2px solid', borderBottom: '2px solid', color: 'black', paddingTop: '0px', paddingBottom: '0px' }}>
-                        <div class="container">
-                            <a class="navbar-brand js-scroll-trigger" href="/"><img
-                                src="Nationwide.png"
-                                width="50"
-                                height="50"
-                                alt="Nationwide Logo"
-                                style={{ borderRadius: '25px' }} /></a>
-
-                            <Form inline>
-                                <FormControl type="text" spellcheck='true' size="sm" placeholder="Search..." className="mr-sm-1" style={{ borderRadius: '25px', width: '250px' }} />
-                            </Form>
-                            <div class="collapse navbar-collapse" id="navbarResponsive">
-
+        if(sessionStorage.getItem('search')==1){
+            return(
+                <div>
+                    <body id="page-top">
+    
+                        <nav class="navbar navbar-expand-lg fixed-top" id="mainNav" style={{ backgroundColor: '#DFDFDF', borderTop: '2px solid', borderBottom: '2px solid', color: 'black', paddingTop: '0px', paddingBottom: '0px' }}>
+                            <div class="container">
+                                <a class="navbar-brand js-scroll-trigger" href="/"><img
+                                    src="Nationwide.png"
+                                    width="50"
+                                    height="50"
+                                    alt="Nationwide Logo"
+                                    style={{ borderRadius: '25px' }} /></a>
+    
+                                <Form onSubmit={this.search} inline>
+                                    <FormControl type="text" id="searchText" spellcheck='true' size="sm" placeholder="Search..." className="mr-sm-1" style={{ borderRadius: '25px', width: '250px' }} />
+                                </Form>
+                                <div class="collapse navbar-collapse" id="navbarResponsive">
+    
+                                </div>
+                            </div>
+                        </nav>
+                    </body>
+                    <br />
+                    <div class="container" style={{ marginTop: '60px', marginBottom: '30px' }}>
+                        <div class="row">
+                            <div class="col-lg-8  ">
+                                <h3 style={{ color: '#3252A4' }}>Search Results:</h3>
+                                <p class="lead"></p>
+                                {
+                                    this.state.filteredResults.map((data, index) =>
+    
+                                        <div>
+                                            <a href="/answer" onClick={() => this.Qstorage(data.question, data.q_id)}>{index + 1}) {data.question}</a><br />
+                                            <br />
+                                        </div>
+                                    )}
                             </div>
                         </div>
-                    </nav>
-                </body>
-                <br />
-
-                <div class="container" style={{ marginTop: '60px', marginBottom: '30px' }}>
-                    <div class="row">
-                        <div class="col-lg-8  ">
-                            <h3 style={{ color: '#3252A4' }}>Trending Questions</h3>
-                            <p class="lead"></p>
-                            {
-                                this.state.TopRatedQ.map((data, index) =>
-
-                                    <div>
-                                        <a href="/answer" onClick={() => this.Qstorage(data.question, data.q_id)}>{index + 1}) {data.question}</a><br />
-                                        <br />
-                                    </div>
-                                )}
+                    
+                       
+                    </div>
+                    <center>
+                        <div>Not found what you were looking for?</div>
+                        <Button variant='danger' onClick={() => this.handleButtonToggleAskModal(true)} style={{ height: '25px', paddingTop: '0' }}>Ask A Question</Button>
+                        </center>
+                        <AskQuestionModal content={this.textAnswer()} title={"Ask A Question"} showModal={this.state.showModal} close={() => this.handleButtonToggleAskModal(false)} /><br /><br />
+                    <footer class="py-1 sticky-bottom" className='FAQFooter'>
+                        <div class="container">
+                            <p class="m-0 text-center text-black">Copyright &copy; APT 2019</p>
+                        </div>
+                    </footer>
+                    </div>
+            )}
+        else if(sessionStorage.getItem('search')==2){
+            return (
+                <div>
+                    <body id="page-top">
+    
+                        <nav class="navbar navbar-expand-lg fixed-top" id="mainNav" style={{ backgroundColor: '#DFDFDF', borderTop: '2px solid', borderBottom: '2px solid', color: 'black', paddingTop: '0px', paddingBottom: '0px' }}>
+                            <div class="container">
+                                <a class="navbar-brand js-scroll-trigger" href="/"><img
+                                    src="Nationwide.png"
+                                    width="50"
+                                    height="50"
+                                    alt="Nationwide Logo"
+                                    style={{ borderRadius: '25px' }} /></a>
+    
+                                <Form onSubmit={this.search} inline>
+                                    <FormControl type="text" id="searchText" spellcheck='true' size="sm" placeholder="Search..." className="mr-sm-1" style={{ borderRadius: '25px', width: '250px' }} />
+                                </Form>
+                                <div class="collapse navbar-collapse" id="navbarResponsive">
+    
+                                </div>
+                            </div>
+                        </nav>
+                    </body>
+                    <br />
+    
+                    <div class="container" style={{ marginTop: '60px', marginBottom: '30px' }}>
+                        <div class="row">
+                            <div class="col-lg-8  ">
+                                <h3 style={{ color: '#3252A4' }}>Trending Questions</h3>
+                                <p class="lead"></p>
+                                {
+                                    this.state.TopRatedQ.map((data, index) =>
+    
+                                        <div>
+                                            <a href="/answer" onClick={() => this.Qstorage(data.question, data.q_id)}>{index + 1}) {data.question}</a><br />
+                                            <br />
+                                        </div>
+                                    )}
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-lg-8 ">
+                                <h3 style={{ color: '#3252A4' }}>Questions Which Need Answers</h3>
+                                <p class="lead"></p>
+                                {
+                                    this.state.UnansweredQ.map((data, index) =>
+                                        <div>
+                                            <a href="/answer" onClick={() => this.Qstorage(data.question, data.q_id)}>{index + 1}) {data.question}</a><br />
+                                            <br />
+                                        </div>
+                                    )}
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-lg-8">
+                                <h3 style={{ color: '#3252A4' }}>Recently Posted Questions</h3>
+                                <p class="lead"></p>
+                                {
+                                    this.state.RecentQ.map((data, index) =>
+                                        <div>
+                                            <a href="/answer" onClick={() => this.Qstorage(data.question, data.q_id)}>{index + 1}) {data.question}</a><br />
+                                            <br />
+                                        </div>
+                                    )}
+    
+                            </div>
                         </div>
                     </div>
-                    <div class="row">
-                        <div class="col-lg-8 ">
-                            <h3 style={{ color: '#3252A4' }}>Questions Which Need Answers</h3>
-                            <p class="lead"></p>
-                            {
-                                this.state.UnansweredQ.map((data, index) =>
-                                    <div>
-                                        <a href="/answer" onClick={() => this.Qstorage(data.question, data.q_id)}>{index + 1}) {data.question}</a><br />
-                                        <br />
-                                    </div>
-                                )}
+                   
+                    <footer class="py-1 sticky-bottom" className='FAQFooter'>
+                        <div class="container">
+                            <p class="m-0 text-center text-black">Copyright &copy; APT 2019</p>
                         </div>
-                    </div>
-                    <div class="row">
-                        <div class="col-lg-8">
-                            <h3 style={{ color: '#3252A4' }}>Recently Posted Questions</h3>
-                            <p class="lead"></p>
-                            {
-                                this.state.RecentQ.map((data, index) =>
-                                    <div>
-                                        <a href="/answer" onClick={() => this.Qstorage(data.question, data.q_id)}>{index + 1}) {data.question}</a><br />
-                                        <br />
-                                    </div>
-                                )}
-
-                        </div>
-                    </div>
+                    </footer>
                 </div>
-               <center>
-                    <Button variant='danger' onClick={() => this.handleButtonToggleAskModal(true)} style={{ height: '25px', paddingTop: '0' }}>Ask A Question</Button>
-                    </center>
-                    <AskQuestionModal content={this.textAnswer()} title={"Ask A Question"} showModal={this.state.showModal} close={() => this.handleButtonToggleAskModal(false)} /><br /><br />
-                <footer class="py-1 sticky-bottom" className='FAQFooter'>
-                    <div class="container">
-                        <p class="m-0 text-center text-black">Copyright &copy; APT 2019</p>
-                    </div>
-                </footer>
-            </div>
-        )
-    }
+            )
+        }
+        }
+       
 }
